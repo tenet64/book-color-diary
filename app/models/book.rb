4644ae86611ed6a9519b5336d423f003eb986907
tenet64 <keigo.ouchida@gmail.com>
2025-09-19 21:14:11 +0900

    fix:UI

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
 validates :title, presence: true,length: {maximum: 100}
 validates :color_code, presence: true
-validates :comment, length: {maximum: 20}, allow_blank: true
+validates :comment, presence: true,length: {maximum: 30}
 end
